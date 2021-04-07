f748019c27c0cc8dfac18ea70edf25f337bd8ec6
Danilo Sousa <danilo.sousa@faculdadeimpacta.com.br>
2021-04-06 22:51:23 -0300

    Fix spaces

--- bi21.py
+++ bi21.py
@@ -12,9 +12,9 @@ all_files = os.listdir(dir)
 txt_files = filter(lambda x: x[-4:] == '.txt', all_files)
 files = list(txt_files)
 
-
+names= {'nome':str,'grupo':str, 'ra':str}
 for filename in list(files):
-    df = pd.read_csv(dir+filename, index_col=False)
+    df = pd.read_csv(dir+filename, dtype=names,index_col=False)
     li.append(df)
 
 
